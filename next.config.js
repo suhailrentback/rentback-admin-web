4d24e0b70e7c6c0319d90bd87b70eebfb88d0921
suhailrentback <suhail@rentback.app>
2025-10-10 14:28:46 +0200

    Update and rename next.config.js to next.config.mjs

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Keep builds green even if ESLint finds issues (we’ll tighten later).
-  eslint: { ignoreDuringBuilds: true },
-  poweredByHeader: false,
-};
-
-export default nextConfig;
