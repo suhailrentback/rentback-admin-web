25eca7213e7a394efa33249c9df33e9a3a858782
suhailrentback <suhail@rentback.app>
2025-10-13 17:54:45 +0200

    Update and rename tailwind.config.cjs to tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+// ADMIN /tailwind.config.ts
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  darkMode: ['class'],
+  theme: { extend: {} },
+  plugins: [],
+} satisfies Config;
